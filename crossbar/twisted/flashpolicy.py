16ecba9d803485f0774c18ae3683b43d09c52ec1
Alexander Herrmann <darignac@gmail.com>
2017-02-21 17:52:43 +0100

    fixed variable conversion error

--- crossbar/twisted/flashpolicy.py
+++ crossbar/twisted/flashpolicy.py
@@ -82,7 +82,7 @@ class FlashPolicyProtocol(Protocol):
             self.dropConnection = None
 
     def dataReceived(self, data):
-        self.received += data.decode()
+        self.received += data.decode('utf-8')
         if FlashPolicyProtocol.REQUESTPAT.match(self.received):
             # got valid request: send policy file
             ##
